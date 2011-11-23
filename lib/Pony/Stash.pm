0ed1c5a6f95cf65732bba319fd0835fa4bb0e86b
Gosha Bugov <gosha.bugov@gmail.com>
2011-11-23 23:46:36 +0600

    add Pony::Stash

---
+++ lib/Pony/Stash.pm
@@ -0,0 +1,105 @@
+package Pony::Stash;
+use Pony::Object 'singleton';
+
+    # R2D2 where are you? 
+
+    use Storable qw(freeze thaw);
+    
+    our $VERSION = '0.000002';
+    
+    has conf => {};
+    has file => '';
+    
+    /**
+     *  Read file and init
+     *  config by data from file.
+     */
+    sub init
+        {
+            my $this = shift;
+               $this->file = shift;
+            
+            open F, $this->file or warn 'Can`t open ' . $this->file;
+            {
+                local $/;
+                
+                my $conf = <F>;
+                
+                $this->conf = ( length $conf ? thaw $conf : {} );
+            }
+            close F;
+        }
+    
+    /**
+     *  Save config into file.
+     */
+    sub save
+        {
+            my $this = shift->new;
+            
+            open  F, '>', $this->file;
+            print F freeze($this->conf);
+            close F;
+        }
+    
+    /**
+     *  Find config and return.
+     *  Create config if not found ... and return result.
+     */
+    sub findOrCreate
+        {
+            my $this = shift->new;
+            my ($name, $conf ) = @_;
+            
+            $this->set($name => $conf) unless exists $this->conf->{$name};
+            
+            return $this->get($name);
+        }
+    
+    /**
+     *  Find config by name and return.
+     */
+    sub get
+        {
+            my $this = shift->new;
+            my ( $name ) = @_;
+            
+            return $this->conf->{$name} if exists $this->conf->{$name};
+            return undef;
+        }
+    
+    /**
+     *  Add or replace hash into $this->conf.
+     */
+    sub set
+        {
+            my $this = shift->new;
+            my ( $name, $conf ) = @_;
+            
+            $this->conf->{$name} = $conf;
+        }
+    
+    /**
+     *  Delete element from config hash.
+     */
+    sub delete
+        {
+            my $this = shift->new;
+            my ( $name ) = @_;
+            
+            delete $this->conf->{$name};
+        }
+    
+1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011, Georgy Bazhukov.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
+
---
+++ t/03-stash.t
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+
+use lib './lib';
+use Test::More tests => 8;
+
+use_ok 'Pony::Object';
+use_ok 'Pony::Stash';
+
+use Pony::Object;
+use Pony::Stash;
+
+clean();
+
+{
+    my $conf = new Pony::Stash('./t/03-stash/config.dat');
+       $conf->set( user => { table   => 'users',
+                             default => 'anonymous' } );
+       
+    my $u = $conf->findOrCreate( user => { table => 'u', default => 'anon' } );
+    
+    ok( 'users' eq $u->{table}, 'findOrCreate (find)' );
+    
+    my $a = $conf->findOrCreate( article => { table => 'article' } );
+    
+    ok( 'article' eq $a->{table}, 'findOrCreate (create)' );
+    
+    $a->{table} = 'art';
+    $conf->set( article => $a );
+    $a = $conf->get('article');
+    
+    ok( 'art' eq $a->{table}, 'update via set' );
+}
+
+my $u = Pony::Stash->get('user');
+
+ok( 'users' eq $u->{table}, 'get from singleton' );
+
+my $stash = new Pony::Stash;
+
+Pony::Stash->save;
+Pony::Stash->new->init('./t/03-stash/stash.dat');
+
+$u = Pony::Stash->findOrCreate( user => { table => 'u', default => 'anon' } );
+
+ok( 'u' eq $u->{table}, 'findOrCreate (create). 2nd stash.' );
+
+Pony::Stash->save;
+Pony::Stash->new->init('./t/03-stash/config.dat');
+
+$u = Pony::Stash->findOrCreate( user => { table => 'u', default => 'anon' } );
+
+ok( 'users' eq $u->{table}, 'findOrCreate (find). Back to 1st stash.' );
+
+clean();
+
+diag( "Testing Pony::Stash $Pony::Stash::VERSION" );
+
+    /**
+     *  Clean up
+     */
+    sub clean
+        {
+            open  C, '>./t/03-stash/config.dat' or die;
+            print C '';
+            close C;
+
+            open  S, '>./t/03-stash/stash.dat' or die;
+            print S '';
+            close S;
+        }
